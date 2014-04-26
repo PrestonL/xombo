02f7aee828c95a3ecdd88bbafb6f3cf187027878
root <root@ip-10-248-78-87.us-west-2.compute.internal>
2014-04-26 08:03:21 +0000

    removed app specific code. still needs clean-up

--- framework/assets/js/site.js
+++ framework/assets/js/site.js
@@ -20,42 +20,6 @@
  * 	limitations under the License.
  */
 var jsLoaded = [];
-var fixIE = function () {
-	// fixes issues w/ IE9
-	$('input[type="submit"], input[type="button"], button').addClass ('button');
-}
-var userBackground = (function (obj) {
-	var background = "";
-	return function (obj) {
-		if (typeof obj == 'undefined') obj = $("body");
-		if (typeof obj.attr ("data-background") != 'undefined' && obj.attr ("data-background") != background) {
-			$("body").css ("background", "#000000 url('" + obj.attr ("data-background") + "') top center");
-			background = obj.attr ("data-background");
-		}
-	};
-})();
-var fixHover = function () {
-	if (!navigator.userAgent.match(/(iPad|iPhone|iPod|BlackBerry|PlayBook|webOS|Android)/i)) {
-		$('form input[type="button"], ' +
-			'form input[type="submit"], ' +
-			'form button.hover, ' +
-			'#column #header div ul li.toggle a, ' +
-			'#column #header div ul li a, ' +
-			'#column #content ul li div, ' +
-			'#column #content ul li.screenshot div').addClass ("hover");
-	}
-}
-var contentResize = function () {
-	if (typeof FB != 'undefined') FB.Canvas.setSize();
-}
-var screenshots = function () {
-	$("li.screenshot").lazybackground ({
-		data_attribute : "screenshot",
-		appear : function () {
-			$(this).find ("div:first").css ("background", "url(" + $(this).attr ("data-screenshot") + ") no-repeat").addClass ("screenshotLoaded");
-		}
-	});
-}
 var ajaxifyLinks = function () {
 	$('a[rel="external"]').attr ("target", "_blank");
 	if ($.xslt.transform () && !navigator.userAgent.match(/(Android|BlackBerry|PlayBook)/i)) {
@@ -136,16 +100,11 @@ var ajaxLoad = function (xml, xsl, target, targetFunc, pushHistory, data) {
 			});
 			ajaxifyLinks ();
 			screenshots ();
-			fixIE ();
-			fixHover ();
 			$("body").css ("cursor", "default");
 			$("a, a *").css ("cursor", "pointer");
 			if (!navigator.userAgent.match(/(iPad|iPhone|iPod|BlackBerry|PlayBook|webOS|Android)/i)) {
 				$(options.target).find ('[title]').tipTip ({ maxWidth: "150px", fadeIn: 0, fadeOut: 0 });
 			}
-			// report to google analytics
-			_gaq.push(['_trackPageview', options.xmlUrl]);
-			contentResize ();
 			return;
 		},
 		xmlCache : false,
@@ -176,55 +135,9 @@ if (typeof history != 'undefined' && typeof history.pushState != 'undefined') $(
 	return true;
 });*/
 $(document).ready (function () {
-	userBackground ();
 	$("noscript").remove ();
-	fixIE ();
-	fixHover ();
 	hashLoad ();
 	if (typeof history != 'undefined' && typeof history.replaceState != 'undefined')
 		history.replaceState ({ href : document.location.href, template : null, target : null }, $("title").text (), document.location.href);
 	ajaxifyLinks ();
-	$("hr").live ("click", function (event) {
-		if (document.location.href.substr (0, 5) == "https") {
-			document.location.href = "http://framework.xombo.com/";
-		} else {
-			ajaxLoad ("/", "/defaultcontroller/xsl", "#content, #header");
-		}
-	});
-	$("#column #dialogue a.cancel[href=\"#\"]").live ("click", function (event) {
-		$("#dialogue").css ("height", $("#dialogue").height ());
-		$("#dialogue").children ().fadeOut (function () {
-			$("#dialogue").slideUp (function () {
-				$(this).empty ();
-				$(this).css ("height", null);
-			});
-		});
-		event.stopPropagation ();
-		event.preventDefault ();
-		return false;
-	});
-	if (!navigator.userAgent.match(/(iPad|iPhone|iPod|BlackBerry|PlayBook|webOS|Android)/i)) {
-		$('[title]').tipTip ({ maxWidth: "150px", fadeIn: 0, fadeOut: 0 });
-	}
-	screenshots ();
-});
-var scrollLoad = true;
-$(window).scroll (function (){
-	if (!scrollLoad) return;
-	scrollLoad = false;
-	count = $("#column #content ul li.shortURL").length;
-	if (count > 0 && $(window).scrollTop () == $(document).height () - $(window).height ()) {
-		ajaxLoad ("/" + (document.location.href.indexOf ("user") > -1 ? "user/profile/" + $(".username").text () : "defaultcontroller/describe") + "/30/" + count, "/defaultcontroller/xsl", "#content", "append");
-		setTimeout (function () {
-			if ($("#column #content ul li.shortURL").length == count) {
-				setTimeout (function () {
-					scrollLoad = true;
-				}, 30000);
-			} else {
-				scrollLoad = true;
-			}
-		}, 5000);
-	} else {
-		scrollLoad = true;
-	}
 });
